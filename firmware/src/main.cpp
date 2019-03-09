724817c31753a1eee6ed67a7b63057e92d0beefa
Matti Airas <mairas@iki.fi>
2019-03-09 22:18:20 +0200

    firmware works now

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -15,35 +15,37 @@
 // Output is varied according to the following logic table:
 //
 // VCC   EN   Out
-//  1     0    H
-//  1     1    H 30s, 0 0.5s
+//  1     0    H 30s, 0 0.5s
+//  1     1    H (This is the default state when the watchdog is running)
 //  0     x    H
 //
 // Pins:
 
 #define SENSE_VCC_PB PB3  // pin 2
-#define EN_INV_PB PB2 // pin 7
+#define EN_PB PB2 // pin 7
 #define INV_SENSE_VCC_PB PB4 // pin 3 (output)
 
-uint8_t input_mask = (1<<SENSE_VCC_PB) & (1<<EN_INV_PB);
+#define INPUT_MASK (_BV(SENSE_VCC_PB) & _BV(EN_PB))
 
 void setup() {
-  // put your setup code here, to run once:
-
   // set all pins to input (high impedance)
   DDRB = 0;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  uint8_t state = PORTB & input_mask;
-  if (state==input_mask) {
-    delay(500);
-    // set to output mode
-    DDRB |= (1<<INV_SENSE_VCC_PB);
-    PORTB &= ~(1 << INV_SENSE_VCC_PB);
-    delay(500);
-    // set to input mode (high impedance) again
-    DDRB &= ~(1<<INV_SENSE_VCC_PB);
+  // test if SENSE_VCC_PB is high and EN_PB low
+  if ((PINB & _BV(SENSE_VCC_PB)) && !(PINB & _BV(EN_PB))) {
+    delay(30000);
+    // if still
+    if ((PINB & _BV(SENSE_VCC_PB)) && !(PINB & _BV(EN_PB))) {
+      // set to output mode
+      DDRB |= _BV(INV_SENSE_VCC_PB);
+      PORTB &= ~_BV(INV_SENSE_VCC_PB);
+      delay(500);
+      // set to input mode (high impedance) again
+      PORTB |= _BV(INV_SENSE_VCC_PB);
+      DDRB &= ~_BV(INV_SENSE_VCC_PB);
+    }
   }
+  delay(100);
 }
