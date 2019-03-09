6db861a2a5bfa68497b15aa76ac8cc43cb8253ce
Matti Airas <mairas@iki.fi>
2019-03-09 22:17:27 +0200

    watchdog pin logic updated

--- src/pi_super_ups/pi_super_ups.py
+++ src/pi_super_ups/pi_super_ups.py
@@ -10,13 +10,15 @@ import time
 
 import RPi.GPIO as GPIO
 
-GPIO_PIN = 3
-HOLD_TIME = 10.0
+SENSE_PIN = 26
+WATCHDOG_PIN = 21
+HOLD_TIME = 2.0
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--sense-pin', type=int, default=GPIO_PIN)
+    parser.add_argument('--sense-pin', type=int, default=SENSE_PIN)
+    parser.add_argument('--watchdog-pin', type=int, default=WATCHDOG_PIN)
     parser.add_argument('--hold-time', type=float, default=HOLD_TIME)
     parser.add_argument('-n', default=False, action='store_true')
 
@@ -26,18 +28,18 @@ def run_state_machine(logger, gpio_pin, hold_time, pretend_only=False):
     state = "START"
     while True:
         if state=="START":
-            if GPIO.input(gpio_pin):
-                # pin high means no power
+            if not GPIO.input(gpio_pin):
+                # pin low means no power
                 logger.warn("Detected blackout on startup")
                 state = "BLACKOUT"
             else:
                 state = "OK"
         elif state=="OK":
-            GPIO.wait_for_edge(gpio_pin, GPIO.RISING)
+            GPIO.wait_for_edge(gpio_pin, GPIO.FALLING)
             logger.warn("Detected blackout")
             state = "BLACKOUT"
         elif state=="BLACKOUT":
-            channel = GPIO.wait_for_edge(gpio_pin, GPIO.FALLING,
+            channel = GPIO.wait_for_edge(gpio_pin, GPIO.RISING,
                                          timeout=int(hold_time*1000))
             if channel is None:
                 # didn't get power back in time
@@ -68,6 +70,7 @@ def main():
     args = parse_arguments()
 
     sense_pin = args.sense_pin
+    watchdog_pin = args.watchdog_pin
     hold_time = args.hold_time
 
     logger = logging.getLogger('pi_super_ups')
@@ -79,6 +82,9 @@ def main():
 
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(sense_pin, GPIO.IN)
+    GPIO.setup(watchdog_pin, GPIO.OUT)
+    # signal the firmware that we're alive
+    GPIO.output(watchdog_pin, 1)
 
     atexit.register(gpio_cleanup)
 
