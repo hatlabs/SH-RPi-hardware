342cebb04fbd3d6e52728445e8c1b4783c44620b
Matti Airas <matti.airas@hatlabs.fi>
2023-02-15 20:52:13 +0200

    v2.0.0

--- prepare_panel_assembly.sh
+++ prepare_panel_assembly.sh
@@ -6,16 +6,15 @@ set -euo pipefail
 echo "Panelizing..."
 
 kikit panelize \
-   --layout 'grid; space: 2mm; rows: 2; cols: 2' \
-   --tabs 'annotation' \
-   --cuts 'mousebites; drill: 0.5mm; spacing: 0.8mm; offset: -0.25mm; prolong: 0.5mm' \
-   --framing 'frame; width: 5mm; space: 2 mm; cuts: both' \
-   --tooling '3hole; hoffset: 12.5mm; voffset: 2.5mm; size: 1.152mm' \
-   --fiducials '3fid; hoffset: 15mm; voffset: 3.85mm; coppersize: 2mm; opening: 1mm;' \
-   --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
-   --post 'millradius: 1mm' \
-   SH-RPi.kicad_pcb SH-RPi-panel.kicad_pcb
-
+    --layout 'grid; rows: 2; cols: 2; space: 5mm' \
+    --tabs 'fixed; vwidth: 55mm; hwidth:46.5mm; vcount: 1; hcount: 1' \
+    --cuts 'vcuts; layer: Edge.Cuts' \
+    --framing 'frame; width: 5mm; space: 2 mm' \
+    --tooling '4hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.152mm' \
+    --fiducials '4fid; hoffset: 5mm; voffset: 3.85mm; coppersize: 2mm; opening: 1mm;' \
+    --text 'simple; text: JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hoffset: 20mm; hjustify: center; vjustify: center;' \
+    --post 'millradius: 1mm' \
+    SH-RPi.kicad_pcb SH-RPi-panel.kicad_pcb
 # ./panelize.py
 
 echo "Generating fabrication files..."
